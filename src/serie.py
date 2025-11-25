355e5d11f821585fbb1318677235b585402cf8d8
Maria Antônia <contatomariatrajano@gmail.com>
2025-11-25 17:58:41 -0300

    Classe Serie Base

--- src/serie.py
+++ src/serie.py
@@ -5,4 +5,36 @@ class Serie(Midia):
     Representa uma série no catálogo.
     Herda comportamento e atributos da classe Midia.
     """
-    pass
+    def __init__(self, titulo, tipo, genero, ano, duracao, classificacao_indicativa, elenco, data_lancamento, status, temporada):
+        super().__init__(titulo, "SERIE", genero, ano, duracao, classificacao_indicativa, elenco, data_lancamento, status)
+        self.temporada = []
+
+    # Encapsulamento
+
+    @property
+    def temporada(self):
+        return self._temporada
+
+    @temporada.setter
+    def temporada(self, temporada):
+        if temporada <= 0:
+            print("Número de temporadas deve ser positivo.")
+        else:
+            self._temporada = temporada
+            
+    # Métodos (ainda não implementados pois precisam da classe Temporada para funcionar)
+
+    def adicionar_temporada():
+        pass
+
+    def total_episodios():
+        pass
+
+    def episodios_assistidos():
+        pass
+
+    def atualizar_status():
+        pass
+
+    def media_nota():
+        pass
