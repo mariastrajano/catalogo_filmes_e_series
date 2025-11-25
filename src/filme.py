a4b00e5484a290245cab2f8fbf88d07e8b8a82a7
Maria Antônia <contatomariatrajano@gmail.com>
2025-11-25 17:23:59 -0300

    Classe Filme Completa

--- src/filme.py
+++ src/filme.py
@@ -5,4 +5,19 @@ class Filme(Midia):
     Representa um filme no catálogo.
     Herda comportamento e atributos da classe Midia.
     """
-    pass
+    def __init__(self, titulo, tipo, genero, ano, duracao, classificacao_indicativa, elenco, status, nota):
+        super().__init__(titulo, "FILME", genero, ano, duracao, classificacao_indicativa, elenco, status)
+        self.nota = nota
+
+# Encapsulamento
+
+    @property
+    def nota(self):
+        return self._nota
+
+    @nota.setter
+    def nota(self, nota):
+        if nota < 0 or nota > 10:
+            print("A nota deve ser entre 0 e 10.")
+        else:
+            self._nota = nota
